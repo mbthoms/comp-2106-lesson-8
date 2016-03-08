cf61149a36137a46bae3891799b362810ee08faf
mbthoms <mbthoms@outlook.com>
2016-03-08 09:37:32 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+//Create public connection string
+module.exports = {
+    //local
+    //'url': 'mongodb://localhost/test'
+
+    //live on mLab
+    'url': 'mongodb://mbthoms:password@ds064278.mlab.com:64278/comp2106'
+};
